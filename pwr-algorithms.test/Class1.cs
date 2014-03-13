f77db10001e26a2ff7a1510c78add8f7c354e8fe
pawelsawicz <pawel@sawicz.eu>
2014-03-13 18:09:55 +0100

    bubble sort implementation

--- pwr-algorithms.test/Class1.cs
+++ pwr-algorithms.test/Class1.cs
@@ -11,6 +11,49 @@ namespace pwr_algorithms.test
     [TestFixture]
     public class Class1
     {   
+        [Test]
+        public void GivenVectorOfRandomNumbersThenBubbleSort()
+        {
+            //arrange
+            int[] vectorToSort = { 4, 10, 3, 1, 1, 6, 99 };
+            int[] expectedVector = { 1, 1, 3, 4, 6, 10, 99 };
+
+            //act
+            int[] result = BubbleSort(vectorToSort);
+
+
+
+            //assert
+            Assert.AreEqual(expectedVector, result);
+        }
+
+        
+
+        private int[] BubbleSort(int[] vectorToSort)
+        {
+            int n = vectorToSort.Length;
+
+            for (int z = 0; n > z; n-- )
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (vectorToSort[i] > vectorToSort[i + 1])
+                    {
+                        int a = vectorToSort[i];
+                        int b = vectorToSort[i + 1];
+                        vectorToSort[i] = b;
+                        vectorToSort[i + 1] = a;
+                        
+                    }
+
+                }
+            }
+
+            return vectorToSort; 
+          
+        }
 
     }
+
+
 }
