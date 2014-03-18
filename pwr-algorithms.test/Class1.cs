693ab30790ce060aca087b4d6918cebf50e06cbe
Piotr <198061@student.pwr.wroc.pl>
2014-03-18 12:47:28 +0100

    Heapsort + Quicksort in code ;)
    zobacz czy jest ok :D

--- pwr-algorithms.test/Class1.cs
+++ pwr-algorithms.test/Class1.cs
@@ -66,6 +66,40 @@ namespace pwr_algorithms.test
 
         }
 
+        [Test]
+        public void GivenVectorOfRandomNumbersThenQuickSort()
+        {
+            //arrange
+            int[] vectorToSort = { 4, 10, 3, 1, 1, 6, 99 };
+            int[] expectedVector = { 1, 1, 3, 4, 6, 10, 99 };
+            int left=0;
+            int right=vectorToSort.Length-1;
+
+            //act
+            int[] result = QuickSort(vectorToSort, left, right);
+
+
+
+            //assert
+            Assert.AreEqual(expectedVector, result);
+        }
+
+        [Test]
+        public void GivenVectorOfRandomNumbersThenHeapSort()
+        {
+            //arrange
+            int[] vectorToSort = { 4, 10, 3, 1, 1, 6, 99 };
+            int[] expectedVector = { 1, 1, 3, 4, 6, 10, 99 };
+
+            //act
+            int[] result = HeapSort(vectorToSort);
+
+
+
+            //assert
+            Assert.AreEqual(expectedVector, result);
+        }
+
         private int[] CountingSort(int[] vectorToSort, int maxValue)
         {
             int[] sortedVector = new int[vectorToSort.Length];
@@ -116,7 +150,92 @@ namespace pwr_algorithms.test
 
             return vectorToSort;
 
-        }        
+        }
+
+        private int[] HeapSort(int[] vectorToSort)
+        {
+            int i, j, k, m, x;
+            int n = vectorToSort.Length;
+
+            for (i = 0; i <= n - 1; i++)
+            {
+                j = i;
+                k = j / 2;
+                x = vectorToSort[i];
+
+                while ((k > 0) && (vectorToSort[k] < x))
+                {
+                    vectorToSort[j] = vectorToSort[k];
+                    j = k;
+                    k = j / 2;
+                }
+
+                vectorToSort[j] = x;
+            }
+
+            for (i = n - 1; i > 1; i--)
+            {
+                int a = vectorToSort[1];
+                int b = vectorToSort[i];
+                vectorToSort[1] = b;
+                vectorToSort[i] = a;
+                j = 1;
+                k = 2;
+
+                while (k < i)
+                {
+                    if ((k + 1 < i) && (vectorToSort[k + 1] > vectorToSort[k]))
+                        m = k + 1;
+                    else
+                        m = k;
+
+                    if (vectorToSort[m] <= vectorToSort[j])
+                        break;
+                    int v = vectorToSort[j];
+                    int w = vectorToSort[m];
+                    vectorToSort[m] = w;
+                    vectorToSort[j] = v;
+                    j = m;
+                    k = j + j;
+                }
+            }
+
+            return vectorToSort;
+
+        }
+
+        private int[] QuickSort(int[] vectorToSort, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int x = vectorToSort[(left + right) / 2];
+
+            do
+            {
+                while (vectorToSort[i] < x)
+                    i++;
+                while (vectorToSort[j] > x)
+                    j--;
+                if (i <= j)
+                {
+                    int a = vectorToSort[i];
+                    int b = vectorToSort[j];
+                    vectorToSort[j] = b;
+                    vectorToSort[i] = a;
+                    i++;
+                    j--;
+                }
+            }
+            while (i <= j);
+
+            if (left < j)
+                QuickSort(vectorToSort, left, j);    //wywołanie funkcji przez samą siebie
+            if (right > i)
+                QuickSort(vectorToSort, i, right);   //tu podobnie jak wyżej
+
+            return vectorToSort;
+
+        }
 
         private int[] BubbleSort(int[] vectorToSort)
         {
