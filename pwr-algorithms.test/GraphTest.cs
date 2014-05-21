375b63efc5b85fbc17de337688a5120e52670322
pawelsawicz <pawel@sawicz.eu>
2014-05-21 22:33:26 +0200

    two methods that calculate minimum and maximum number of edges from given number of vertex

---
+++ pwr-algorithms.test/GraphTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace pwr_algorithms.test
+{
+    [TestFixture]
+    public class GraphTest
+    {
+        /*
+         * undirected weighted graph 
+         */
+
+        [Test]
+        public void GivenNumberOfVertexThenReturnMinimumNumberOfEdges()
+        {
+            int numberOfVertex = 5;
+            int expectedNumberOfEdges = 4;
+
+            int result = numberOfVertex - 1;
+
+            Assert.AreEqual(expectedNumberOfEdges, result);
+        }
+
+        [Test]
+        public void GivenNumberOfVertexThenReturnMaximumNumberOfEdges()
+        {
+            int numberOfVertex = 4;
+            int expectedNumberOfEdges = 6;
+
+            int result = (numberOfVertex * (numberOfVertex - 1)) / 2;
+
+            Assert.AreEqual(expectedNumberOfEdges, result);
+        }
+    }
+}
